e0cec9f8155d4b2f5213f5bb3f22a3178d594e75
Полина <pechenkova.polina.mgu@yandex.ru>
2024-10-15 04:24:10 +0300

    Также реализован forwardRef на input и кнопке

--- react-app/src/components/SearchInput/SearchInput.jsx
+++ react-app/src/components/SearchInput/SearchInput.jsx
@@ -1,38 +1,3 @@
-// import styles from "./SearchInput.module.css";
-
-// function SearchInput({
-//   img,
-//   placeholder,
-//   value,
-//   onChange,
-//   onKeyDown,
-//   onButtonClick,
-// }) {
-//   return (
-//     <form className={`${styles['form-wrapper']}`} onSubmit={(e) => e.preventDefault()}>
-//       {img && (
-//         <button type="button" className={`${styles['input-button']}`} onClick={onButtonClick}>
-//           <img src={img} alt="Картинка поиска" className={`${styles['input-img']}`} />
-//         </button>
-//       )}
-//       <input
-//         ref={ref}
-//         type="text"
-//         name="text"
-//         placeholder={placeholder}
-//         value={value}
-//         onChange={onChange}
-//         className={styles.input}
-//         onKeyDown={onKeyDown}
-//       />
-//     </form>
-//   );
-// }
-
-// export default SearchInput;
-
-
-
 import React, { forwardRef } from "react";
 import styles from "./SearchInput.module.css";
 
